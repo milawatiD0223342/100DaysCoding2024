254a0cfa054593c6840e6a2a464890f7dfd64ca9
Mila <mwati9879@gmail.com>
2024-09-02 22:49:31 +0800

    hari 1

---
+++ Hari001/Day1.java
@@ -0,0 +1,28 @@
+package Hari001;
+
+public class Day1 {
+    public static void main(String[] args) {
+   
+        // Macam-macam cara menampilkan teks
+ 
+        System.out.print("Ini print ");
+        System.out.print("Halo");
+        System.out.println("Hello World!");
+        
+        String nama = "Mila";
+        int umur = 20;
+        
+        /*
+        
+        "f" dalam printf mengartikan "format"
+        
+        aljaldladk
+        
+        alkladkl
+        
+        */
+        
+        System.out.printf("Nama: %s, Umur: %d\n", nama, umur);
+    
+    }
+}
