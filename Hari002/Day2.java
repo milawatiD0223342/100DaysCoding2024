71d05a2bbb3ee8ce7a51f98ba122052b3b1d065a
Mila <mwati9879@gmail.com>
2024-09-03 20:12:40 +0800

    hari2

---
+++ Hari002/Day2.java
@@ -0,0 +1,10 @@
+package Hari002;
+
+public class Day2 {
+    public static void main(String[] args) {
+          int angka = 8;
+          int angka1 = 3;
+          int angka2 = angka+angka1;
+        System.out.println(angka2);
+    }
+}
