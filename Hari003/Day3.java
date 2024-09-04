4f019a170c486edf66b38bd5bc2532b5d8845308
Mila <mwati9879@gmail.com>
2024-09-04 18:55:44 +0800

    Day3

---
+++ Hari003/Day3.java
@@ -0,0 +1,12 @@
+package Hari003;
+
+public class Day3 {
+    public static void main(String[] args) {
+        String nama = "Mila";
+        String alamat = "Limboro";
+        int umur = 20;
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Umur: " + umur + " tahun");
+    }
+}
