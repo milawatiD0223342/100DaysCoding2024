7dfb6b6bfdc14714d7a466e09d473a84e8b0e7b3
Mila <mwati9879@gmail.com>
2024-09-06 06:31:06 +0800

    Day5

---
+++ Hari005/Day5.java
@@ -0,0 +1,13 @@
+package Hari005;
+
+public class Day5 {
+    public static void main(String[] args) {
+        // komentar baris tunggal
+        System.out.println("Mila");
+       /* komentar multi baris
+        */
+       /**komentar dokumentasi
+        * ajkjak
+        */
+    }
+}
