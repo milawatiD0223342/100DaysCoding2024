508c5de32ec9c35ea3914d0c0b956aaddbe5dbfb
Mila <mwati9879@gmail.com>
2024-09-05 15:25:53 +0800

    Day4

---
+++ Hari004/Day4.java
@@ -0,0 +1,17 @@
+package Hari004;
+
+public class Day4 {
+
+    public static void main(String[] args) {
+        int nilai = 20;
+        System.out.println(nilai);
+        if (nilai == 21) {
+            System.out.println("ini benar");
+        } else if (nilai == 22) {
+            System.out.println("ini salah");
+        }else{
+            System.out.println("semuanya salah");
+        }
+
+    }
+}
