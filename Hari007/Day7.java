c3bc4be8149ef22c777d259e1bf59ad984dbbd83
Mila <mwati9879@gmail.com>
2024-09-08 05:48:37 +0800

    Day7

---
+++ Hari007/Day7.java
@@ -0,0 +1,11 @@
+package Hari007;
+
+public class Day7 {
+    public static void main(String[] args) {
+        String nama, alamat;
+        nama = "Mila";
+        alamat = "Limboro";
+        System.out.println("Nama :" + nama);
+        System.out.println("Alamat :" + alamat);
+    }
+}
