8a6b20f98c6cdb4c9ddf1a89f2f1a31eadd45304
Mila <mwati9879@gmail.com>
2024-09-09 05:51:15 +0800

    Day8

--- Hari007/Day7.java
+++ Hari007/Day7.java
@@ -3,6 +3,7 @@ package Hari007;
 public class Day7 {
     public static void main(String[] args) {
         String nama, alamat;
+        // inisialisasi
         nama = "Mila";
         alamat = "Limboro";
         System.out.println("Nama :" + nama);
---
+++ Hari008/Day8.java
@@ -0,0 +1,9 @@
+package Hari008;
+
+public class Day8 {
+    public static void main(String[] args) {
+        int angka = 15;
+        angka = 13; // mengupdate variabel yg awalnya 15 jadi 13
+        System.out.println(angka);
+    }
+}
