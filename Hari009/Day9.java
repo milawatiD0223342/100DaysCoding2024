ad12b77b1c2c05231ea3de7a1af811c07fe524d0
Mila <mwati9879@gmail.com>
2024-09-10 19:22:04 +0800

    Day9

---
+++ Hari009/Day9.java
@@ -0,0 +1,8 @@
+package Hari009;
+
+public class Day9 {
+    public static void main(String[] args) {
+       final double PHI = 3.14; //variabel constanta: nilai tdk dpt di ubah dan sebelum type data tambahka modifier/keyword final
+        System.out.println("PHI : "+PHI);
+    }
+}
