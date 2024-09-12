65f310788cf97d6b3a7e6e4f8d0cc74259e9f471
Mila <mwati9879@gmail.com>
2024-09-13 05:29:03 +0800

    Day12

---
+++ Hari012/Day12.java
@@ -0,0 +1,10 @@
+package Hari012;
+
+public class Day12 {
+    public static void main(String[] args) {
+        int angka1 = 12;
+        int angka2 = 14;
+        System.out.println(angka1);
+        System.out.println(angka2);
+    }
+}
