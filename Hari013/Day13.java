0a2f8440606557e296f142eb5fa6ff8102b3c0c1
Mila <mwati9879@gmail.com>
2024-09-14 17:55:37 +0800

    Day13

---
+++ Hari013/Day13.java
@@ -0,0 +1,13 @@
+package Hari013;
+
+public class Day13 {
+    public static void main(String[] args) {
+         int angka1 = 5;
+         int angka2 = 10;
+         int angka = angka1;
+         angka1 = angka2;
+         angka2 = angka;
+         System.out.println("angka1 : " +angka1);
+         System.out.println("angka2 : " +angka2);
+    }
+}
