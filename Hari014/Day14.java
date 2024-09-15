3ac0d101023cd36be7924c91cd2814c7748f92d9
Mila <mwati9879@gmail.com>
2024-09-15 10:17:31 +0800

    Day14

--- Hari014/Day14.java
+++ Hari014/Day14.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Hari014;
 
-/**
- *
- * @author mwati
- */
 public class Day14 {
-    
+    public static void main(String[] args) {
+        long saldoRekening = 100000L;
+        System.out.println(saldoRekening);
+    }
 }
