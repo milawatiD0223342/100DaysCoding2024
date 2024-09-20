54a81d3c9929eb64e4985c216089f44ada005fc9
Mila <mwati9879@gmail.com>
2024-09-20 10:20:04 +0800

    Day19

---
+++ Hari019/Day19.java
@@ -0,0 +1,14 @@
+package Hari019;
+
+public class Day19 {
+    public static void main(String[] args) {
+    String nama, alamat;
+    nama = "Mila";
+    alamat = "Palleroang";
+    int a = nama.length();
+        System.out.println(nama.length());
+        System.out.println(alamat.replace('o','i'));
+    
+    }
+   }
+
