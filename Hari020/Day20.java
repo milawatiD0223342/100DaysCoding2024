74dd7780a254f6c148422cebe8060b382dfc5eb4
Mila <mwati9879@gmail.com>
2024-09-21 07:42:57 +0800

    Day20

---
+++ Hari020/Day20.java
@@ -0,0 +1,10 @@
+package Hari020;
+
+public class Day20 {
+    public static void main(String[] args) {
+        Byte a = 5;
+        Byte b = 3;
+        System.out.println(a.byteValue());
+        System.out.println(a.compareTo(b));
+    }
+}
