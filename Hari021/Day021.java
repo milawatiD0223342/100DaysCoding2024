a85917292b4bdc73594be2e720729250c3275d3c
Mila <mwati9879@gmail.com>
2024-09-22 16:09:08 +0800

    Day21

---
+++ Hari021/Day021.java
@@ -0,0 +1,11 @@
+package Hari021;
+
+public class Day021 {
+    public static void main(String[] args) {
+        Short b = 4;
+        Short c = 5;
+        String str = Short.toString(c);
+        System.out.println(b.shortValue());
+        System.out.println(str);
+    }
+}
