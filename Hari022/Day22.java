21271f3dd1c5a308ce34cbd2a15cc141566b05f3
Mila <mwati9879@gmail.com>
2024-09-23 18:36:21 +0800

    Day22

---
+++ Hari022/Day22.java
@@ -0,0 +1,10 @@
+package Hari022;
+
+public class Day22 {
+    public static void main(String[] args) {
+        Integer a = 24;
+        System.out.println(a.intValue());
+        Integer b = 25;
+        System.out.println(b.compareTo(a));
+    }
+}
