17cb2b4e001def0f6c6e7fdda524ff1a8adb6e07
Mila <mwati9879@gmail.com>
2024-09-24 06:49:37 +0800

    Day23

---
+++ Hari023/Day23.java
@@ -0,0 +1,10 @@
+package Hari023;
+
+public class Day23 {
+    public static void main(String[] args) {
+        Float a = 1.5f;
+        Float b = 1.6f;
+        System.out.println(a.floatValue());
+        System.out.println(a.compareTo(b));
+    }
+}
