96b4b4290a4c0ad8d2a984907f6ea2dcf15c1723
Mila <mwati9879@gmail.com>
2024-09-27 07:42:37 +0800

    Day26

---
+++ Hari026/Day26.java
@@ -0,0 +1,10 @@
+package Hari026;
+
+public class Day26 {
+    public static void main(String[] args) {
+        Character inisial = 'M';
+        Character nomor = '3';
+        System.out.println(inisial.charCount(inisial));
+        System.out.println(nomor.charValue());
+    }
+}
