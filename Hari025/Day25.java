1fbfa5e38a25598b6262ec23782d088cddad4a9c
Mila <mwati9879@gmail.com>
2024-09-27 00:54:03 +0800

    Dat25

---
+++ Hari025/Day25.java
@@ -0,0 +1,10 @@
+package Hari025;
+
+public class Day25 {
+    public static void main(String[] args) {
+        Double a = 2.5;
+        Double b = 3.5;
+        System.out.println(a.toString());
+        System.out.println(b.compareTo(b));
+    }
+}
