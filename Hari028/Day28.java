0e94b66992f0a2d74b952c28a9ce792a2f223f2d
Mila <mwati9879@gmail.com>
2024-09-29 05:55:10 +0800

    Day28

---
+++ Hari028/Day28.java
@@ -0,0 +1,10 @@
+package Hari028;
+
+public class Day28 {
+    public static void main(String[] args) {
+       Boolean a = true;
+       Boolean b = false;
+       System.out.println(a.booleanValue());
+        System.out.println(a.equals(b));
+    }
+}
