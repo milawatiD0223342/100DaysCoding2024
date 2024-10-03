716e06ac8a7a62b8a3b87e5f2e5be7dae6fdb97f
Mila <mwati9879@gmail.com>
2024-10-03 19:38:57 +0800

    Day32

---
+++ Hari032/Day32.java
@@ -0,0 +1,30 @@
+package Hari032;
+
+public class Day32 {
+    public static void main(String[] args) {
+        byte a = 1;
+        short b = 2;
+        int c = 20;
+        long d = 21;
+        float e = 2.2f;
+        double f = 2.3;
+        char g = '2';
+        boolean h = false;
+        String i = Byte.toString(a);
+        System.out.println(i);
+        String j = Short.toString(b);
+        System.out.println(j);
+        String k = Integer.toString(c);
+        System.out.println(k);
+        String l = Long.toString(d);
+        System.out.println(l);
+        String m = Float.toString(e);
+        System.out.println(m);
+        String n = Double.toString(f);
+        System.out.println(n);
+        String o = Character.toString(g);
+        System.out.println(o);
+        String p = Boolean.toString(h);
+        System.out.println(p);
+    }
+}
