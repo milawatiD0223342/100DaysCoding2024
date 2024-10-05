c8dd5221afdb287b40e572d4903e8556f6306b64
Mila <mwati9879@gmail.com>
2024-10-05 18:51:31 +0800

    Day34

---
+++ Hari034/Day34.java
@@ -0,0 +1,15 @@
+package Hari034;
+
+import java.util.Scanner;
+
+public class Day34 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nama : ");
+        String nama = sc.nextLine();
+        System.out.println("Nama saya : "+nama);
+        System.out.print("Masukkan angka ganjil : ");
+        int angka = sc.nextInt();
+        System.out.println("Angka ganjil : "+angka);
+    }
+}
