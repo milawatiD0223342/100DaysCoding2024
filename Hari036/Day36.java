be26475586bf47b37418a1abbd71ac77ae31f43c
Mila <mwati9879@gmail.com>
2024-10-07 21:05:32 +0800

    Day36

---
+++ Hari036/Day36.java
@@ -0,0 +1,15 @@
+package Hari036;
+
+public class Day36 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c,d,e;
+        c = a * b;
+        d = a / b;
+        e = a % b;
+        System.out.println("Perkalian a * b = "+c);
+        System.out.println("Pembagian a / b = "+d);
+        System.out.println("Sisa bagi a % b = "+e);
+    }
+}
