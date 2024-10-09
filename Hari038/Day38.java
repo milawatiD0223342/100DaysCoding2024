77e06a48ceb019ba13ffe2d165aeed768d3e4466
Mila <mwati9879@gmail.com>
2024-10-09 18:31:43 +0800

    Day38

---
+++ Hari038/Day38.java
@@ -0,0 +1,20 @@
+package Hari038;
+
+public class Day38 {
+    public static void main(String[] args) {
+        int a = 60;
+        int b = 30;
+        int c;
+        System.out.println("CARA PERTAMA");
+        c = a * b;
+        System.out.println("a * b = " + c);
+        c = a / b;
+        System.out.println("a / b = " + c);
+        int d = 20;
+        System.out.println("\nCARA KEDUA");
+        d *= 5;
+        System.out.println("a * b = " + d);
+        d /= 5;
+        System.out.println("a / b = " + d);
+    }
+}
