7018d92db23a15e28b75ceeec303b2596ca4b4d0
Mila <mwati9879@gmail.com>
2024-10-10 20:27:47 +0800

    Day39

---
+++ Hari039/Day39.java
@@ -0,0 +1,12 @@
+package Hari039;
+
+public class Day39 {
+
+    public static void main(String[] args) {
+        int a, b;
+        a = 8;
+        b = 5;
+        a %= b;
+        System.out.println("Sisa bagi dari 8/5 = " + a);
+    }
+}
