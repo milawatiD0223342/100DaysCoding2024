2ac07434fd6e2a8d00fa2fbcd08b0421ed8863ea
Mila <mwati9879@gmail.com>
2024-10-11 15:04:00 +0800

    Day40

---
+++ Hari040/Day40.java
@@ -0,0 +1,14 @@
+package Hari040;
+
+public class Day40 {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 15;
+        boolean c;
+        c = a > b;
+        System.out.println("a > b = "+c);
+        c = a < b;
+        System.out.println("a < b = "+c);
+    }
+}
