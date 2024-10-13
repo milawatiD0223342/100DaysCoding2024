2d5513b80a2bee6d19adbeffd7c88e146122a513
Mila <mwati9879@gmail.com>
2024-10-13 09:33:02 +0800

    Day42

---
+++ Hari042/Day42.java
@@ -0,0 +1,14 @@
+package Hari042;
+
+public class Day42 {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 15;
+        boolean c;
+        c = a == b;
+        System.out.println(c);
+        c = a != b;
+        System.out.println(c);
+    }
+}
