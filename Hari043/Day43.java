055014188224eafa3ae81222407b9915520911ed
Mila <mwati9879@gmail.com>
2024-10-14 19:58:47 +0800

    day43

---
+++ Hari043/Day43.java
@@ -0,0 +1,14 @@
+package Hari043;
+
+public class Day43 {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 15;
+        boolean c;
+        c = a >= b;
+        System.out.println(c);
+        c = a <= b;
+        System.out.println(c);
+    }
+}
