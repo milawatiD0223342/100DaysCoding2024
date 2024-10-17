af67afb50f77dcdd3f74d4c2e0ee5369eca7a157
Mila <mwati9879@gmail.com>
2024-10-18 07:21:16 +0800

    Day47

---
+++ Hari047/Day47.java
@@ -0,0 +1,12 @@
+package Hari047;
+
+public class Day47 {
+
+    public static void main(String[] args) {
+        int a = 200;
+        if (a > 200) {
+            System.out.println("Benar");
+        }
+        System.out.println("Salah");
+    }
+}
