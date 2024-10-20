90bac92a57b8395aef503d0e87e6a2d81693f994
Mila <mwati9879@gmail.com>
2024-10-20 10:13:20 +0800

    Dy49

---
+++ Hari049/Day49.java
@@ -0,0 +1,13 @@
+package Hari049;
+
+public class Day49 {
+
+    public static void main(String[] args) {
+        int a = 200;
+        if (a > 100) {
+            System.out.println("Benar");
+        } else if (a < 200) {
+            System.out.println("Salah");
+        }
+    }
+}
