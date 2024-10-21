6cf2b4b53153939aaa9a8ec0d56c749c0fd788d4
Mila <mwati9879@gmail.com>
2024-10-21 19:43:28 +0800

    Day50

--- Hari050/Day50.java
+++ Hari050/Day50.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Hari050;
 
-/**
- *
- * @author mwati
- */
 public class Day50 {
-    
+
+    public static void main(String[] args) {
+        int a = 200;
+        if (a > 300) {
+            System.out.println("Benar");
+        } else if (a < 200) {
+            System.out.println("Salah");
+        } else {
+            System.out.println("Semuanya Salah");
+        }
+    }
 }
