f63c9d60ef148345102af54c2a44e8b87b44c025
Mila <mwati9879@gmail.com>
2024-10-23 23:45:27 +0800

    Day52

---
+++ Hari052/Day52.java
@@ -0,0 +1,11 @@
+package Hari052;
+
+public class Day52 {
+
+    public static void main(String[] args) {
+        boolean a = false;
+        String b;
+        b = a ? "iya" : "tidak";
+        System.out.println(b);
+    }
+}
