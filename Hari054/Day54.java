6ac9e7028b11a1b262320a6f043b6f5dd59ccdbe
Mila <mwati9879@gmail.com>
2024-10-25 23:20:44 +0800

    Day54

--- Hari054/Day54.java
+++ Hari054/Day54.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Hari054;
 
-/**
- *
- * @author mwati
- */
 public class Day54 {
-    
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
 }
