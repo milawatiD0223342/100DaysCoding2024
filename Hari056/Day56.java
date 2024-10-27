7be9792210dce7ef482fe03caf4f9ffd546a4efb
Mila <mwati9879@gmail.com>
2024-10-27 20:30:21 +0800

    Day56

---
+++ Hari056/Day56.java
@@ -0,0 +1,10 @@
+package Hari056;
+
+public class Day56 {
+
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
