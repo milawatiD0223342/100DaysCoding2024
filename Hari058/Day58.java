fa1e5f804dde7374221694222814418443eed5ed
Mila <mwati9879@gmail.com>
2024-10-29 17:58:32 +0800

    Day58

---
+++ Hari058/Day58.java
@@ -0,0 +1,12 @@
+package Hari058;
+
+public class Day58 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        while (a >= 1) {
+            System.out.print(a + " ");
+            a--;
+        }
+    }
+}
