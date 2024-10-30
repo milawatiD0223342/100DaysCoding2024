45e73be2e773d4133a63d644820bd6e5dbe6bdbe
Mila <mwati9879@gmail.com>
2024-10-30 17:32:25 +0800

    Day59

---
+++ Hari059/Day59.java
@@ -0,0 +1,11 @@
+package Hari059;
+
+public class Day59 {
+
+    public static void main(String[] args) {
+        int[] a = {2, 3};
+        for (int b : a) {
+            System.out.print(b + " ");
+        }
+    }
+}
