50aeffbd39ba85dd4cfc64f783e8fbd06544a119
Mila <mwati9879@gmail.com>
2024-10-31 19:37:59 +0800

    Day60

---
+++ Hari060/Day60.java
@@ -0,0 +1,12 @@
+package Hari060;
+
+public class Day60 {
+
+    public static void main(String[] args) {
+        int a = 1;
+        do {
+            System.out.print(a + " ");
+            a++;
+        } while (a <= 10);
+    }
+}
