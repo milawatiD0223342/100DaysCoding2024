bc03f277a32637ccbb9a33f94a4809b04da39b99
Mila <mwati9879@gmail.com>
2024-11-02 19:39:12 +0800

    Day62

---
+++ Hari062/Day62.java
@@ -0,0 +1,13 @@
+package Hari062;
+
+public class Day62 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            if (i == 3) {
+                break;
+            }
+            System.out.println(i + " ");
+        }
+    }
+}
