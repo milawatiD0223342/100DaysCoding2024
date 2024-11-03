b1170f3cc835ef23a05d931d1a63eed4cc647f04
Mila <mwati9879@gmail.com>
2024-11-03 20:26:25 +0800

    Day63

---
+++ Hari063/Day63.java
@@ -0,0 +1,13 @@
+package Hari063;
+
+public class Day63 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            if (i == 3) {
+                continue;
+            }
+            System.out.println(i + " ");
+        }
+    }
+}
