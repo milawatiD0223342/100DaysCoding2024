d2ea716c2298dee340a8bb6210c3c0804a96ed42
Mila <mwati9879@gmail.com>
2024-11-04 19:33:13 +0800

    Day64

--- Hari064/Day64.java
+++ Hari064/Day64.java
@@ -1,7 +1,16 @@
 package Hari064;
 
 public class Day64 {
+
     public static void main(String[] args) {
-        
+        int i = 2;
+        loop:
+        while (i < 10) {
+            if (i == 6) {
+                break loop;
+            }
+            System.out.println("i = " + i);
+            i++;
+    }
     }
 }
