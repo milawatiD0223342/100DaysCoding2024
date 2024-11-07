51165e00766fc3a39f9b7303ad0f49df36895307
Mila <mwati9879@gmail.com>
2024-11-07 21:35:57 +0800

    Day67

---
+++ Hari067/Day67.java
@@ -0,0 +1,12 @@
+package Hari067;
+
+public class Day67 {
+
+    public static void nama() {
+        System.out.println("Mila");
+    }
+
+    public static void main(String[] args) {
+        nama();
+    }
+}
