ec9cb8bd019b19ef10a8fc6ef72c8bc4fa6c76ee
Mila <mwati9879@gmail.com>
2024-11-08 22:58:06 +0800

    Day68

---
+++ Hari068/Day68.java
@@ -0,0 +1,16 @@
+package Hari068;
+
+public class Day68 {
+
+    public static int jumlah(int a, int b) {
+        int hasil = a + b;
+        return hasil;
+    }
+
+    public static void main(String[] args) {
+        int c = 2;
+        int d = 3;
+        int hasilJumlah = jumlah(c, d);
+        System.out.println(hasilJumlah);
+    }
+}
