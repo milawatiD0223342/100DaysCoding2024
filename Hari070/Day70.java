b69e8dd3db79bdfc39e3c76b8b6da691fda8c165
Mila <mwati9879@gmail.com>
2024-11-10 18:35:15 +0800

    Day70

---
+++ Hari070/Day70.java
@@ -0,0 +1,13 @@
+package Hari070;
+
+public class Day70 {
+
+    public static void myTugas(int a, int b) {
+        int c = a + b;
+        System.out.println(c);
+    }
+
+    public static void main(String[] args) {
+        myTugas(2, 3);
+    }
+}
