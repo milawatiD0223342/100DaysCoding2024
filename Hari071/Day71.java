1da5c8d367d0a7e35ca6221d40369875eb5ee0d4
Mila <mwati9879@gmail.com>
2024-11-11 22:59:00 +0800

    Day71

---
+++ Hari071/Day71.java
@@ -0,0 +1,12 @@
+package Hari071;
+
+public class Day71 {
+
+    static void methodStatic() {
+        System.out.println("Mila");
+    }
+
+    public static void main(String[] args) {
+        methodStatic();
+    }
+}
