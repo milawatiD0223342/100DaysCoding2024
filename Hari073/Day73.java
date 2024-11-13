98c4919bf7089ffb161a977e777dd3da0e8218c2
Mila <mwati9879@gmail.com>
2024-11-13 23:30:40 +0800

    Day73

---
+++ Hari073/Day73.java
@@ -0,0 +1,10 @@
+package Hari073;
+
+public class Day73 {
+
+    public static void main(String[] args) {
+        String a = "Mila";
+        char b = a.charAt(0);
+        System.out.println(b);
+    }
+}
