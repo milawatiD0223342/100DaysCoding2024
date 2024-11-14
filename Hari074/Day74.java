0eb5328013af22373316a496ce112da7bae1b5e5
Mila <mwati9879@gmail.com>
2024-11-14 13:23:32 +0800

    Day74

---
+++ Hari074/Day74.java
@@ -0,0 +1,10 @@
+package Hari074;
+
+public class Day74 {
+
+    public static void main(String[] args) {
+        String nama = "Milawati";
+        int a = nama.length();
+        System.out.println("Panjang karakter = " + a);
+    }
+}
