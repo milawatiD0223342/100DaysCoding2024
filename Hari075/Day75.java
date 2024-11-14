3ece7fe615308f2221c5cf0af7f55e5929d811fd
Mila <mwati9879@gmail.com>
2024-11-15 07:58:40 +0800

    Day75

---
+++ Hari075/Day75.java
@@ -0,0 +1,10 @@
+package Hari075;
+
+public class Day75 {
+
+    public static void main(String[] args) {
+        String a = "Kuliah ternyata tidak seindah yang ku lihat di FTV";
+        String b = a.toLowerCase();
+        System.out.println(b);
+    }
+}
