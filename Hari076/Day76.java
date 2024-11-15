488f774530a25f34c064d45b7051ed23c454373a
Mila <mwati9879@gmail.com>
2024-11-16 05:05:41 +0800

    Day76

---
+++ Hari076/Day76.java
@@ -0,0 +1,10 @@
+package Hari076;
+
+public class Day76 {
+
+    public static void main(String[] args) {
+        String a = "Kuliah ternyata tidak seindah yang ku lihat di FTV";
+        String b = a.toUpperCase();
+        System.out.println(b);
+    }
+}
