07f71d84c6651a9ab5fc0a95647d3745fb06071f
Mila <mwati9879@gmail.com>
2024-11-17 16:54:45 +0800

    Day77

---
+++ Hari077/Day77.java
@@ -0,0 +1,12 @@
+package Hari077;
+
+public class Day77 {
+
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "Hello";
+        String c = "What?";
+        System.out.println(a.equals(c));
+        System.out.println(a.equals(b));
+    }
+}
