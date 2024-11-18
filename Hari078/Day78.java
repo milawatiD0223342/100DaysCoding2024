ce289206228b36534d1dc887845f1b44c0f63706
Mila <mwati9879@gmail.com>
2024-11-18 18:42:25 +0800

    Day78

---
+++ Hari078/Day78.java
@@ -0,0 +1,12 @@
+package Hari078;
+
+public class Day78 {
+
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "HeLLo";
+        String c = "What?";
+        System.out.println(a.equalsIgnoreCase(b));
+        System.out.println(a.equalsIgnoreCase(c));
+    }
+}
