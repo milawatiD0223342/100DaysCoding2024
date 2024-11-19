dd78d0043819bb749d13ac21331cc501f8f091ba
Mila <mwati9879@gmail.com>
2024-11-19 18:44:47 +0800

    Day79

---
+++ Hari079/Day79.java
@@ -0,0 +1,9 @@
+package Hari079;
+
+public class Day79 {
+
+    public static void main(String[] args) {
+        String a = new String("Kuliah capek, gk guliah yh gk S.T");
+        System.out.println(a.toString());
+    }
+}
