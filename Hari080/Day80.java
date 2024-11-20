b2d3b5154c752f2b5050b6c41c0fab1406dddc04
Mila <mwati9879@gmail.com>
2024-11-20 20:02:26 +0800

    Day80

---
+++ Hari080/Day80.java
@@ -0,0 +1,10 @@
+package Hari080;
+
+public class Day80 {
+
+    public static void main(String[] args) {
+        String a = "       Mila siapa?";
+        String b = a.trim();
+        System.out.println(b);
+    }
+}
