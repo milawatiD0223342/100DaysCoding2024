630195e536c9d39c812f5d8467280698508ae0ef
Mila <mwati9879@gmail.com>
2024-11-21 19:59:27 +0800

    Day81

---
+++ Hari081/Day81.java
@@ -0,0 +1,13 @@
+package Hari081;
+
+public class Day81 {
+
+    public static void main(String[] args) {
+        int a = 230;
+        double b = 3.14;
+        boolean c = true;
+        System.out.println(String.valueOf(a));
+        System.out.println(String.valueOf(b));
+        System.out.println(String.valueOf(c));
+    }
+}
