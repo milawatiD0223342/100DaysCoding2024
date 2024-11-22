5ca1a1ebb5c4b741bc59a643f59366e20ae85729
Mila <mwati9879@gmail.com>
2024-11-22 08:30:28 +0800

    Day82

---
+++ Hari082/Day82.java
@@ -0,0 +1,14 @@
+package Hari082;
+
+public class Day82 {
+
+    public static void main(String[] args) {
+        double a = 3.5;
+        double ceil = Math.ceil(a);
+        System.out.println("ceil : " + ceil);
+        double floor = Math.floor(a);
+        System.out.println("floor : " + floor);
+        long round = Math.round(a);
+        System.out.println("round : " + round);
+    }
+}
