6ca6cc3aa06900bbc1f7a33d3d10eb20bd271085
Mila <mwati9879@gmail.com>
2024-11-23 21:50:53 +0800

    Day83

---
+++ Hari083/Day83.java
@@ -0,0 +1,11 @@
+package Hari083;
+
+public class Day83 {
+
+    public static void main(String[] args) {
+        int[] array = {3, 4, 5};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
