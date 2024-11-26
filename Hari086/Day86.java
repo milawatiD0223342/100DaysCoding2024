c6f72dff2bebc0221cec5a13ecb5cbc45aaabb55
Mila <mwati9879@gmail.com>
2024-11-26 18:05:13 +0800

    Day86

---
+++ Hari086/Day86.java
@@ -0,0 +1,11 @@
+package Hari086;
+
+public class Day86 {
+
+    public static void main(String[] args) {
+        String[] array = {"Mila", "Aulia", "Putri"};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
