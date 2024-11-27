61400556c647ce09e420fdde18a7a9273c4c2ad7
Mila <mwati9879@gmail.com>
2024-11-27 17:44:56 +0800

    Day87

---
+++ Hari087/Day87.java
@@ -0,0 +1,12 @@
+package Hari087;
+
+public class Day87 {
+
+    public static void main(String[] args) {
+        String[] array = {"Mila", "Aulia", "Putri"};
+        array[1] = "Lia";
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
