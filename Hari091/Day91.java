63d7e1579e629748ce0097b108b346be312ea980
Mila <mwati9879@gmail.com>
2024-12-01 16:41:06 +0800

    Day91

---
+++ Hari091/Day91.java
@@ -0,0 +1,18 @@
+package Hari091;
+
+import java.util.Scanner;
+
+public class Day91 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input : ");
+        int a = sc.nextInt();
+        System.out.print("Output : ");
+        if (a >= 20) {
+            System.out.println("You and i");
+        } else {
+            System.out.println("end");
+        }
+    }
+}
