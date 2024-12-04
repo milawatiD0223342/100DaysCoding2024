8c4c06d1cd17402f6b1be86fcdb8fdf864879a5f
Mila <mwati9879@gmail.com>
2024-12-04 19:01:52 +0800

    Day94

---
+++ Hari094/Day94.java
@@ -0,0 +1,17 @@
+package Hari094;
+
+public class Day94 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
